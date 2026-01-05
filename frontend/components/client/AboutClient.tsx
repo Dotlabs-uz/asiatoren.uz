7e792ae8066d19a01d45400d68f2eca830be2c59
MuniS97 <tursunovmunis141@gmail.com>
2026-01-05 17:15:35 +0500

    minor fix

--- frontend/components/client/AboutClient.tsx
+++ frontend/components/client/AboutClient.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
+import WorldMap from "../ui/world-map";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -196,14 +197,40 @@ export const AboutClient = ({ translations }: AboutClientProps) => {
                 </div>
 
                 {/* Map */}
-                <div className="about-map w-full aspect-video lg:aspect-21/9 rounded-2xl md:rounded-3xl overflow-hidden bg-gray-100">
-                    <Image
-                        src="/images/map.png"
-                        alt="World Map"
-                        width={5000}
-                        height={5000}
-                        loading="lazy"
-                        className="w-full h-full object-contain bg-white"
+                <div className=" dark:bg-black bg-white w-full">
+                    <WorldMap
+                        dots={[
+                            {
+                                start: {
+                                    lat: 64.2008,
+                                    lng: -149.4937,
+                                }, // Alaska (Fairbanks)
+                                end: {
+                                    lat: 34.0522,
+                                    lng: -118.2437,
+                                }, // Los Angeles
+                            },
+                            {
+                                start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
+                                end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                            },
+                            {
+                                start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                                end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
+                            },
+                            {
+                                start: { lat: 51.5074, lng: -0.1278 }, // London
+                                end: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                            },
+                            {
+                                start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                                end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
+                            },
+                            {
+                                start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                                end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
+                            },
+                        ]}
                     />
                 </div>
             </div>
